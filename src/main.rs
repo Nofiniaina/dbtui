8fcf3ce046cdc01834db2fc366aa2764e60b52c5
Nofiniaina <nofyrnd@gmail.com>
2026-01-02 13:51:42 +0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use ratatui::{DefaultTerminal, Frame};
+use std::io;
+
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    ratatui::run(app)?;
+    Ok(())
+}
+
+fn app(terminal: &mut DefaultTerminal) -> io::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("Welcome to DBTUI", frame.area());
+}
